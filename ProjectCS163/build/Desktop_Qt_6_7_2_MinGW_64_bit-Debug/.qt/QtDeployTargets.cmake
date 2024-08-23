417e82e9f7036818c441ef286f1baf0224c9847e
ThanhPhat1509 <161999845+ThanhPhat1509@users.noreply.github.com>
2024-08-23 14:22:38 +0700

    update edit definition

--- ProjectCS163/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ProjectCS163_FILE D:/BT/C++/0HCMUS/Year 1/S3/0PROJECT/ver8/ProjectCS163/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ProjectCS163.exe)
-set(__QT_DEPLOY_TARGET_ProjectCS163_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ProjectCS163_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
